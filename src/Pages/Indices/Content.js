d66bd1fdd1be8015e78babeb4002f40f4a1a6e1b
Jithin K Satheesh <jithinksatheesh@gmail.com>
2022-04-02 22:13:26 +0530

    push

--- src/Pages/Indices/Content.js
+++ src/Pages/Indices/Content.js
@@ -9,8 +9,8 @@ export default function Container(props) {
         <div className="container mx-auto py-16">
             <div className="pb-16">
                 <div className="flex justify-between text-dark-blue">
-                    <div className="text-base px-12 py-5 border-b-2 border-dark-blue">Dimension Armenia AMD Bond Index</div>
-                    <div className="text-base px-12 py-5">Dimension Armenia USD Corporate Bond Index</div>
+                    <div className="text-base px-12 cursor-pointer py-5 border-b-2 border-dark-blue">Dimension Armenia AMD Bond Index</div>
+                    <div className="text-base px-12 cursor-pointer py-5">Dimension Armenia USD Corporate Bond Index</div>
                 </div>
             </div>
             <div className="flex">
